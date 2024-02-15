ecbc597a1e106d942d91e7309b83eaa267eabc0e
amel100k <eaa23ema@students.eaaa.dk>
2024-02-15 12:49:04 +0100

    Lavet nogle opgaver

--- rekursion/Opgave03.java
+++ rekursion/Opgave03.java
@@ -4,40 +4,40 @@ import java.lang.reflect.Array;
 
 public class Opgave03 {
     public static void main(String[] args) {
-        int[] intArray = new int[]{1,2,3,4,5,6};
+        int[] intArray = new int[]{1, 2, 3, 4, 5, 6};
         //System.out.println("findHeltalIArray(intArray,3) = " + findHeltalIArray(intArray, 3));
-        System.out.println("findHeltalIArray2(intArray,3) = " + findHeltalIArray2(intArray, 3));
+        System.out.println("findHeltalIArray2(intArray,3) = " + findHeltalIArray2(intArray, 2));
     }
-    public static boolean findHeltalIArray(int[] array, int target){
+
+    public static boolean findHeltalIArray(int[] array, int target) {
         boolean found = false;
         int i = 0;
-        while (!found && i < array.length){
+        while (!found && i < array.length) {
             int k = array[i];
-            if (k == target){
+            if (k == target) {
                 found = true;
-            }
-            else i++;
+            } else i++;
         }
         return found;
     }
-    public static boolean findHeltalIArray2(int[] array, int target){
+
+    public static int findHeltalIArray2(int[] array, int target) {
         int low = 0;
         int high = array.length - 1;
-        return findHeltalIArray2(array, target);
+        return findHeltalIArray2(array, target, low, high);
     }
+
     private static int findHeltalIArray2(int[] array, int target, int low, int high) {
         int result = 0;
         if (low > high) {
             result = -low - 1;
-            int middle = (low + high) / 2;
-            if (target < array[middle]) {
-                result = findHeltalIArray2(array, target, low, middle - 1);
-            }
-            else if (target == array[middle]) {
-                result = middle;
-            }
-            else result = findHeltalIArray2(array, target, middle + 1, high);
         }
+        int middle = (low + high) / 2;
+        if (target < array[middle]) {
+            result = findHeltalIArray2(array, target, low, middle - 1);
+        } else if (target == array[middle]) {
+            result = middle;
+        } else result = findHeltalIArray2(array, target, middle + 1, high);
         return result;
     }
 }
